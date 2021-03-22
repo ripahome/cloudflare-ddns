cc9d9b6a42250a73e59d160f48efc65bf7dfd45d
ripahome <81170213+ripahome@users.noreply.github.com>
2021-03-22 15:06:16 +0100

    Update docker-build.sh

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -1,2 +1,6 @@
+# #!/bin/bash
+# docker build --platform linux/amd64 --tag timothyjmiller/cloudflare-ddns:latest ../
+
 #!/bin/bash
-docker build --platform linux/amd64 --tag timothyjmiller/cloudflare-ddns:latest ../
+docker buildx build --platform  linux/ppc64le,linux/s390x,linux/386,linux/arm/v6,linux/arm/v7,linux/arm64/v8,linux/amd64 --tag timothyjmiller/cloudflare-ddns:latest ../
+# TODO: Support linux/riscv64
